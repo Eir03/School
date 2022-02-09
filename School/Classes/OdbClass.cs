8e11bb3ce1e3ad98b254ff8a442c1530794893a3
Eir03 <staroverovdanila@gmail.com>
2022-02-09 18:01:35 +0300

    Добавил проект

---
+++ School/Classes/OdbClass.cs
@@ -0,0 +1,14 @@
+﻿using School.ModelDataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Classes
+{
+    class OdbClass
+    {
+        public static Eir_SchoolEntities entities;
+    }
+}
---
+++ School/Pages/PageMain.xaml.cs
@@ -0,0 +1,42 @@
+﻿using School.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace School.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+            DG.ItemsSource = OdbClass.entities.Student.ToList();
+            
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            FrameClass.frm.GoBack();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
